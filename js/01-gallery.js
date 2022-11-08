31840c13543b1c0c234147fd499b8e69c77cc865
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-08 13:40:32 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+console.log(galleryItems[1].original);
+
+const gallery = document.querySelector(".gallery");
+
+for (let i = 0; i < galleryItems.length; i++) {
+  const div = document.createElement("div");
+  gallery.append(div);
+  const imagesElements = div.insertAdjacentHTML(
+    "beforeend",
+    `<img url=${galleryItems[i].original} alt= "${galleryItems[i].description}"/>`
+  );
+}
+console.log(gallery);
