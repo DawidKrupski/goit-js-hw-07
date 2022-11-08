296ebeb515d9badbbc176d1f1e6db47027fc8770
DawidKrupski <dawid.krupski323@gmail.com>
2022-11-08 15:02:47 +0100

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,27 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems[1].original);
-
 const gallery = document.querySelector(".gallery");
+
+const clickElement = (event) => {
+  event.preventDefault();
+};
+
+gallery.addEventListener("click", clickElement);
 
 for (let i = 0; i < galleryItems.length; i++) {
   const div = document.createElement("div");
   gallery.append(div);
-  const imagesElements = div.insertAdjacentHTML(
+  const link = document.createElement("a");
+  div.append(link);
+  link.insertAdjacentHTML(
     "beforeend",
-    `<img url=${galleryItems[i].original} alt= "${galleryItems[i].description}"/>`
+    `<img 
+    class="gallery__image"
+    src=${galleryItems[i].preview}
+    data-source=${galleryItems[i].original} alt= "${galleryItems[i].description}"/>`
   );
+  div.className = "gallery__item";
+  link.className = "gallery__link";
+  link.href = `${galleryItems[i].original}`;
 }
-console.log(gallery);
